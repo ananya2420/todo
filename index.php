72e4e590a1f17d3fc9455f19804c6079ca1cfc2b
ananya2420 <114337926+ananya2420@users.noreply.github.com>
2023-08-23 23:15:06 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<HTML LANG="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name ="viewpoint" content="width-device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1 class="top-heading">TODO list application</h1>
+        <form action="" method="post">
+            <div class="input-container">
+                <input type="text" name="inputBox" id="inputBox">
+                <button type="submit" id="add">ADD</button>
+            </div>
+            <ul class="list">
+                <li class="item fade">
+                    <p class="deleted-text"><span>Pay Electricity Bill</span></p>
+                    <button type ="submit" name="" id="check"><i class="fa-solid fa-check"></i></button>
+                    <button type ="submit" name="" id="delete"><i class="fa-solid fa-trash-can"></i></button>
+                </li>
+            </ul>
+            <hr>
+            <ul class="list">
+                <li class="item">
+                    <p>Finish Summer Reading List</p>
+                    <button type ="submit" name="" id="check"><i class="fa-solid fa-check"></i></button>
+                    <button type ="submit" name="" id="delete"><i class="fa-solid fa-trash-can"></i></button>
+                </li>
+            </ul>
+            
+            <script src="https://example.com/fontawesome/v5.15.4/js/all.js" data-auto-replace-svg="nest"></script>
+
+
+        </form>
+    </div>
+</body>
+</HTML>
